b87b23acc41e6a4e38bf443dd7da28257b174507
Jelle Besseling <jelle@pingiun.com>
2020-10-19 19:06:04 +0200

    Use correct cachix repo

--- nix/default.nix
+++ nix/default.nix
@@ -42,10 +42,34 @@ in
       overrides = poetry2nix.overrides.withDefaults (
         _self: super: {
           pillow = super.pillow.overridePythonAttrs (
-            old: {
-              setupPyBuildFlags = "--disable-xcb";
-              nativeBuildInputs = [ pkgs.pkgconfig ] ++ old.nativeBuildInputs;
-              buildInputs = with pkgs; [ freetype libjpeg openjpeg zlib libtiff libwebp tcl lcms2 ] ++ old.buildInputs;
+            old: with pkgs; {
+              preConfigure =
+                let
+                  libinclude' = pkg: ''"${pkg.out}/lib", "${pkg.out}/include"'';
+                  libinclude = pkg: ''"${pkg.out}/lib", "${pkg.dev}/include"'';
+                in
+                ''
+                  sed -i "setup.py" \
+                      -e 's|^FREETYPE_ROOT =.*$|FREETYPE_ROOT = ${libinclude freetype}|g ;
+                          s|^JPEG_ROOT =.*$|JPEG_ROOT = ${libinclude libjpeg}|g ;
+                          s|^JPEG2K_ROOT =.*$|JPEG2K_ROOT = ${libinclude openjpeg}|g ;
+                          s|^IMAGEQUANT_ROOT =.*$|IMAGEQUANT_ROOT = ${libinclude' libimagequant}|g ;
+                          s|^ZLIB_ROOT =.*$|ZLIB_ROOT = ${libinclude zlib}|g ;
+                          s|^LCMS_ROOT =.*$|LCMS_ROOT = ${libinclude lcms2}|g ;
+                          s|^TIFF_ROOT =.*$|TIFF_ROOT = ${libinclude libtiff}|g ;
+                          s|^TCL_ROOT=.*$|TCL_ROOT = ${libinclude' tcl}|g ;
+                          s|self\.disable_platform_guessing = None|self.disable_platform_guessing = True|g ;'
+                  export LDFLAGS="-L${libwebp}/lib"
+                  export CFLAGS="-I${libwebp}/include"
+                ''
+                # Remove impurities
+                + stdenv.lib.optionalString stdenv.isDarwin ''
+                  substituteInPlace setup.py \
+                    --replace '"/Library/Frameworks",' "" \
+                    --replace '"/System/Library/Frameworks"' ""
+                '';
+              nativeBuildInputs = [ pkgconfig ] ++ old.nativeBuildInputs;
+              buildInputs = [ freetype libjpeg openjpeg zlib libtiff libwebp tcl lcms2 ] ++ old.buildInputs;
             }
           );
           uwsgi = { };
